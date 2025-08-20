2a94910535523d619d82317213351dd98d33d9ee
Kong Yao <kongyao12345@gmail.com>
2025-08-20 17:37:40 +0800

    Update main app

--- src/main/scala-3/nutriquest/MainApp.scala
+++ src/main/scala-3/nutriquest/MainApp.scala
@@ -1,12 +1,20 @@
 package nutriquest
 
+import javafx.fxml.FXMLLoader
 import nutriquest.model.game.GameManager
 import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.Includes.*
+import scalafx.scene.Scene
+
+import java.net.URL
 
 object MainApp extends JFXApp3:
   override def start(): Unit =
-    println("Starting game controller...")
-    val controller = GameManager()
-    controller.startNewGame()
-    println("Game initialized successfully!")
-    println("Ready for GUI integration!")
+    val rootLayoutResource: URL = getClass.getResource("/nutriquest/view/GameView.fxml")
+    val loader = new FXMLLoader(rootLayoutResource)
+    val rootLayout = loader.load[javafx.scene.layout.BorderPane]()
+    stage = new PrimaryStage:
+      title = "NutriQuest🍌"
+      scene = new Scene:
+        root = rootLayout
