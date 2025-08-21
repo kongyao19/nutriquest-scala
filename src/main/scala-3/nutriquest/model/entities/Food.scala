852b1b5f877b8a909c6a4e52a204c98ff5f89296
Kong Yao <kongyao12345@gmail.com>
2025-08-21 16:20:57 +0800

    Update food class

--- src/main/scala-3/nutriquest/model/entities/Food.scala
+++ src/main/scala-3/nutriquest/model/entities/Food.scala
@@ -1,24 +1,59 @@
 package nutriquest.model.entities
 
-import nutriquest.model.{Collectible, Drawable, NutritionValue, Position}
+import nutriquest.model.{Collectible, NutritionTypes}
 
-abstract class Food(
-                   protected var position: Position,
-                   val nutritionValue: NutritionValue,
-                   val points: Int,
-                   val name: String,
-                   ) extends Drawable, Collectible:
-  protected var collected: Boolean = false
+import scala.util.Random
 
-  def getPosition: Position = position
+// General Food class
+abstract class Food(_x: Double, _y: Double, _imagePath: String, val nutritionType: NutritionTypes, val pointValue: Int)
+  extends GameEntity(_x, _y, _imagePath) with Collectible:
 
-  def isCollected: Boolean = collected
+  def getPointValue: Int = pointValue
 
-  def collect(): Unit = collected = true
+  def getNutritionType: NutritionTypes = nutritionType
 
-  def getDescription: String
+// Healthy Food class
+class HealthyFood(x: Double, y: Double, nutritionType: NutritionTypes)
+  extends Food(x, y, getImagePath(nutritionType), nutritionType, getPointValue(nutritionType)):
 
   def onCollect(player: Player): Unit =
-    player.addScore(points)
-    player.addNutrition(nutritionValue)
-    collect()
+    player.addScore(pointValue)
+
+object HealthyFood:
+  def apply(x: Double, y: Double, nutritionType: NutritionTypes): HealthyFood =
+    new HealthyFood(x, y, nutritionType)
+
+  // Generate random healthy food
+  def generateRandom(x: Double, y: Double): HealthyFood =
+    val healthyTypes = Array(NutritionTypes.Protein, NutritionTypes.Vitamin,
+      NutritionTypes.Carbs, NutritionTypes.Healthy_Fats)
+    val randomType = healthyTypes(Random.nextInt(healthyTypes.length))
+    new HealthyFood(x, y, randomType)
+
+// Unhealthy Food class  
+class UnhealthyFood(x: Double, y: Double)
+  extends Food(x, y, "junk_food.png", NutritionTypes.Junk, -10):
+
+  def onCollect(player: Player): Unit =
+    player.addScore(pointValue) // Negative points
+
+object UnhealthyFood:
+  def apply(x: Double, y: Double): UnhealthyFood = new UnhealthyFood(x, y)
+
+  def generateRandom(x: Double, y: Double): UnhealthyFood =
+    new UnhealthyFood(x, y)
+
+// Helper functions for food generation
+private def getImagePath(nutritionType: NutritionTypes): String = nutritionType match
+  case NutritionTypes.Protein => "protein.png"
+  case NutritionTypes.Vitamin => "vitamin.png"
+  case NutritionTypes.Carbs => "carbs.png"
+  case NutritionTypes.Healthy_Fats => "healthy_fats.png"
+  case NutritionTypes.Junk => "junk_food.png"
+
+private def getPointValue(nutritionType: NutritionTypes): Int = nutritionType match
+  case NutritionTypes.Protein => 15
+  case NutritionTypes.Vitamin => 12
+  case NutritionTypes.Carbs => 10
+  case NutritionTypes.Healthy_Fats => 18
+  case NutritionTypes.Junk => -10
