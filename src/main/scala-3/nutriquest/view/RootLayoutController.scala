41b60d2bdf1b493607d3f9dca51615103063d26f
Kong Yao <kongyao12345@gmail.com>
2025-08-22 19:16:34 +0800

    Implement mnemonics

--- src/main/scala-3/nutriquest/view/RootLayoutController.scala
+++ src/main/scala-3/nutriquest/view/RootLayoutController.scala
@@ -2,13 +2,28 @@ package nutriquest.view
 
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.{Alert, ButtonType}
 import nutriquest.MainApp
+import nutriquest.util.Database
 
 @FXML
 class RootLayoutController:
   @FXML
   def handleClose(action: ActionEvent): Unit =
     System.exit(0)
+
+  @FXML
+  def handleReset(action: ActionEvent): Unit =
+    val alert = new Alert(Alert.AlertType.CONFIRMATION)
+    alert.initOwner(MainApp.stage)
+    alert.setTitle("Clear Leaderboard")
+    alert.setHeaderText("Are you sure you want to clear all scores?")
+    alert.setContentText("This action cannot be undone.")
+    val result = alert.showAndWait()
+    if result.isPresent && result.get() == ButtonType.OK then {
+        Database.clearLeaderboard()
+        MainApp.showMainMenu()
+    }
 
   @FXML
   def handleAbout(action: ActionEvent): Unit =
