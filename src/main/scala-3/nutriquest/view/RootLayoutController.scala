a0231246b6912b7b7ff3a29ee048459248e142f8
Kong Yao <kongyao12345@gmail.com>
2025-08-22 10:35:51 +0800

    Create fxml files

--- src/main/scala-3/nutriquest/view/RootLayoutController.scala
+++ src/main/scala-3/nutriquest/view/RootLayoutController.scala
@@ -1,7 +1,15 @@
 package nutriquest.view
 
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import nutriquest.MainApp
 
 @FXML
 class RootLayoutController:
-  
+  @FXML
+  def handleClose(action: ActionEvent): Unit =
+    System.exit(0)
+
+  @FXML
+  def handleAbout(action: ActionEvent): Unit =
+    MainApp.showInstructions()
